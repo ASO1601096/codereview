2765e40b68ce50032a5c29904bc87bff96c8fefd
Kohei Honda <1601096@st.asojuku.ac.jp>
2018-01-15 16:12:53 +0900

    comment fixed

--- Kadai04-master/Kadai04-master/kadai04.c
+++ Kadai04-master/Kadai04-master/kadai04.c
@@ -23,7 +23,7 @@ static void comp_write(char* out_str,const char str, int num){
 	char comp_str[4]={0};
 	comp_str[0]='@';
 	comp_str[1]=str;
-	comp_str[2]=num+'0';                                /*数字を文字に変更する*/
+	comp_str[2]=num+'0';                                 /*数字を文字に変更する*/
 	strcat(out_str,comp_str);                            /*作成した文字列を出力ファイルに貼り付ける*/	
 	
 }
@@ -40,7 +40,7 @@ static void as_write(char* out_str,const char str, int num){
 		j++;
 		num--;
 	}
-	strcat(out_str,cons_str);                /*作成した文字列を出力ファイルに貼り付ける*/
+	strcat(out_str,cons_str);                            /*作成した文字列を出力ファイルに貼り付ける*/
 }
 /*                                               */
 /*              メイン関数                       */
@@ -51,43 +51,43 @@ int main(void){
 	char in_04[MAX]={0};
 	char out_04[MAX]={0};	
 	char str;
-	FILE *fp;	                             /* (1)ファイルポインタの宣言 */
+	FILE *fp;	                                         /* (1)ファイルポインタの宣言 */
 	
-	fp = fopen("in_04.txt","w");      	     /*ファイルを書き込みモードで開く*/
+	fp = fopen("in_04.txt","w");      	                 /*ファイルを書き込みモードで開く*/
 	
 	if(fp==NULL){
-	    printf("ファイルオープン失敗\n");	 /*ファイルオープンに失敗した場失敗と表示し終了*/
+	    printf("ファイルオープン失敗\n");	             /*ファイルオープンに失敗した場失敗と表示し終了*/
 	    return -1;
 	}
 
 	printf("文字列を入力してください：\n");
-	gets(in_04);                             /*入力した文字列を受け取る*/
+	gets(in_04);                                         /*入力した文字列を受け取る*/
 	
-	fprintf(fp,"%s",in_04);                  /*文字列をファイルに書き込む*/
+	fprintf(fp,"%s",in_04);                              /*文字列をファイルに書き込む*/
 
 	while(in_04[i]!='\0'){
 		str=in_04[i];
-		number=serial_check(in_04,str,i);    /*strの文字の連続をチェック*/
+		number=serial_check(in_04,str,i);                /*strの文字の連続をチェック*/
 		if(number<4){
-			as_write(out_04,str,number);     /*そのままout_04ファイルに書き込む*/
+			as_write(out_04,str,number);                 /*そのままout_04ファイルに書き込む*/
 		}else{
-			comp_write(out_04,str,number);   /*圧縮文字列をout_04ファイルに書き込む*/
+			comp_write(out_04,str,number);               /*圧縮文字列をout_04ファイルに書き込む*/
 		}
-		i+=number;                           /*次の文字の位置に移動する*/
+		i+=number;                                       /*次の文字の位置に移動する*/
 	}
 	
 	fclose(fp);
 	
-	fp = fopen("out_04.txt","w+");           /*ファイルを書き込みモードで開く*/
+	fp = fopen("out_04.txt","w+");      	             /*ファイルを書き込みモードで開く*/
 	
 	if(fp==NULL){
-	    printf("ファイルオープン失敗\n");	 /*ファイルオープンに失敗した場失敗と表示し終了*/
+	    printf("ファイルオープン失敗\n");	             /*ファイルオープンに失敗した場失敗と表示し終了*/
 	    return -1;
 	}
 
-	printf("%s",out_04);                     /*圧縮した文字列を表示*/
+	printf("%s",out_04);                                 /*圧縮した文字列を表示*/
 	
-	fprintf(fp,"%s",out_04);                 /*文字列をファイルに書き込む*/
+	fprintf(fp,"%s",out_04);                             /*文字列をファイルに書き込む*/
 	fclose(fp);
 
 	return (0);
